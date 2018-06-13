e825b8651c316b2117d89bd7c3005cf46eac16e7
David Stewart <davidstewy@gmail.com>
2018-06-13 16:16:46 -0400

    finished changes

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-const output = document.getElementById("output")
+const outputQuestion = document.getElementById("outputQuestion");
+let answerInput;
+const buttonSubmit = document.getElementById("submitAnswer");
+let answerOutput;
+let userScore = document.getElementById("userScore");
+let questionBundle = {};
+let score = 0;
 
 // fetch("http://jservice.io/api/random")
 //   .then(responseObject => responseObject.json())
-//   .then(hydratedBody => let hydratedBody = hydratedBody)
+//   .then(hydratedBody => console.log(hydratedBody))
 
 
 
 
 
 fetch('http://jservice.io/api/random')
-  .then(function(responseObject) {
+  .then(function (responseObject) {
     return responseObject.json();
   })
-  .then(function(hydratedBody) {
-    console.log(hydratedBody)
-    
-    console.log(hydratedBody.question);
-
+  .then(function (hydratedBody) {
+    console.log(hydratedBody[0]);
+    questionBundle = hydratedBody[0];
+    let question = document.createTextNode(questionBundle.question);
+    outputQuestion.appendChild(question);
   });
+
+function checkAnswer() {
+  answerInput = document.getElementById("textInput").value;
+  console.log(answerInput, "--", questionBundle.answer)
+  if (answerInput == questionBundle.answer) {
+
+    score = score + questionBundle.value;
+    let scoreToAppend = document.createTextNode(score);
+    userScore.appendChild(scoreToAppend);
+    alert("That is.....Correct! You've earned " + questionBundle.value + " points!");
+    fetch('http://jservice.io/api/random')
+      .then(function (responseObject) {
+        return responseObject.json();
+      })
+      .then(function (hydratedBody) {
+        console.log(hydratedBody[0]);
+        questionBundle = hydratedBody[0];
+        let question = document.createTextNode(questionBundle.question);
+        outputQuestion.appendChild(question);
+      });
+  } else {
+    alert("So close! You can do it! Please try again", answerInput);
+  }
+}
