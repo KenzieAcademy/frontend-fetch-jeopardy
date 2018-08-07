065cd39be91d0034f10033e7bf6b1c336d78a86a
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-08-07 13:14:00 -0400

    done

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
 const output = document.getElementById("output")
+let answer = "";
+const input = document.createElement("input")
+const score = 0;
+output.appendChild(input)
+let counter = 0;
 
+const checkAnswer = () => {   
+             
+        if(input.value === answer ){
+                counter += 200;
+              let correct = document.createTextNode("  " + counter)
+              output.appendChild(correct)
 
-fetch("http://jservice.io/api/random")
-    .then(responseObject => responseObject.json())
-    .then(hydratedBody => { 
-            console.log(hydratedBody)
-            output.dataset.charaterId = hydratedBody.id
-            output.querySelector("h1").textContent = `${hydratedBody[0].question}`
-            const input = document.createElement("input")
-            output.appendChild(input)
-            const button = document.createElement("button")
-            let buttonText = document.createTextNode("answer")
-            button.appendChild(buttonText)
-            output.appendChild(button)
-            button.dataset.id = button
-    })
+        }else{
+                let wrong = document.createTextNode("wrong  " + counter) 
+                output.appendChild(wrong)
 
+        }
 
+}
+        console.log(counter)
 
+const fetchData = () => {
 
-button.addEventListener("click", )
+        fetch("http://jservice.io/api/random")
+        .then(responseObject => responseObject.json())
+        .then(hydratedBody => { 
+                output.dataset.charaterId = hydratedBody.id
+                output.querySelector("h1").textContent = `${hydratedBody[0].question}`
+                answer = `${hydratedBody[0].answer}`
+                console.log(answer)
+        })             
+}
+fetchData();
+
+const button = document.createElement("button")
+const buttonText = document.createTextNode("answer")
+button.appendChild(buttonText)
+output.appendChild(button)
+button.addEventListener("click", checkAnswer)
+
+const fetchButton = document.createElement("button")
+const fetchButtonText = document.createTextNode("new question")
+fetchButton.appendChild(fetchButtonText)
+output.appendChild(fetchButton)
+fetchButton.addEventListener("click", fetchData)
+        
+        
+
+                                                
